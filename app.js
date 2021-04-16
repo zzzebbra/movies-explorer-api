b739e0cad800bf1c7b0bed580427f10afba352cc
zzzebbra <zzzebbra@yandex.ru>
2021-04-16 03:09:33 +0300

    fixes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-// const cors = require('cors');
+const cors = require('cors');
 const { errors, Joi, celebrate } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 require('dotenv').config();
@@ -16,20 +16,20 @@ const auth = require('./middlewares/auth');
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
-  max: 100, // можно совершить максимум 100 запросов с одного IP
+  max: 1000, // можно совершить максимум 100 запросов с одного IP
 });
 
-// const corsOptions = {
-//   origin: [
-//     'http://localhost:3000', 'http://localhost:3001',
+const corsOptions = {
+  origin: [
+    'http://localhost:3000', 'http://localhost:3001',
 
-//   ],
-//   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-//   preflightContinue: false,
-//   optionsSuccessStatus: 204,
-//   allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
-//   credentials: true,
-// };
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  allowedHeaders: ['Content-Type', 'origin', 'Authorization'],
+  credentials: true,
+};
 
 const {
   PORT = 3000, DB_SERVER = 'localhost', DB_PORT = 27017, DB_NAME = 'movies_explorer',
@@ -47,7 +47,7 @@ app.use(limiter);
 
 app.use(helmet());
 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 
@@ -55,7 +55,7 @@ app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().email().required(),
+    email: Joi.string().email({ ignoreLength: true }).required(),
     password: Joi.string().min(8).required(),
   }),
 }), login);
@@ -63,7 +63,7 @@ app.post('/signin', celebrate({
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    email: Joi.string().email().required(),
+    email: Joi.string().email({ ignoreLength: true }).required(),
     password: Joi.string().min(8).required(),
   }),
 }), createUser);
